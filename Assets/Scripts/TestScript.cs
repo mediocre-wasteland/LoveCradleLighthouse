7bafd3d56af718826a9b54420667715e3465d1f6
ABirch98 <Adam.birch@workmail.com>
2019-01-31 15:40:31 +0000

    Testing
    test

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,20 @@
+﻿//test script
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		//empty
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//empty
+	}
+}
